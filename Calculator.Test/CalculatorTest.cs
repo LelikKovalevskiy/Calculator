230672bfbe6862637707d5eefa3120ef36bcec29
AlekseyKovalevsky <aleksey.kovalevsky1@gmail.com>
2015-10-13 18:32:30 +0300

    initial commit

---
+++ Calculator.Test/CalculatorTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Test
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void AdditionTest()
+        {
+            double a = 10.5, b = 15, result, expectedResult = 25.5;
+            char operation = '+';
+
+            result = Calculator.Calculate(operation, a, b);
+
+            Assert.AreEqual(expectedResult, result);
+
+        }
+        [TestMethod]
+        public void SubtractionTest()
+        {
+            double a = 20.3, b = 4, result, expectedResult = 16.3;
+            char operation = '-';
+
+            result = Calculator.Calculate(operation, a, b);
+
+            Assert.AreEqual(expectedResult, result);
+
+        }
+        [TestMethod]
+        public void MultiplicationTest()
+        {
+            double a = 10.5, b = 15, result, expectedResult = 157.5;
+            char operation = '*';
+
+            result = Calculator.Calculate(operation, a, b);
+
+            Assert.AreEqual(expectedResult, result);
+
+        }
+        [TestMethod]
+        public void DivisionTest()
+        {
+            double a = 20, b = 43, result, expectedResult = 20 / 43.0;
+            char operation = '/';
+
+            result = Calculator.Calculate(operation, a, b);
+
+            Assert.AreEqual(expectedResult, result);
+
+        }
+        [ExpectedException(typeof(DivideByZeroException))]
+        [TestMethod]
+        public void DivideByZeroTest()
+        {
+            double a = Double.MaxValue, b = 0;
+            char operation = '/';
+
+            Calculator.Calculate(operation, a, b);
+        }
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void InvalidOperationTest()
+        {
+            double a = 15, b = 18;
+            char operation = '|';
+
+            Calculator.Calculate(operation, a, b);
+        }
+    }
+}
---
+++ Calculator/Calculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Calculator
+{
+    public static class Calculator
+    {
+        public static double Calculate(char operation, double a, double b)
+        {
+            switch (operation)
+            {
+                case '+':
+                    return Add(a, b);
+                case '-':
+                    return Subtract(a, b);
+                case '*':
+                    return Multiply(a, b);
+                case '/':
+                    return Divide(a, b);
+                default:
+                    throw new ArgumentException("Wrong operation");
+            }
+        }
+
+        private static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        private static double Subtract(double a, double b)
+        {
+            return Add(a, -b);
+        }
+
+        private static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        private static double Divide(double a, double b)
+        {
+            if (Math.Abs(b - 0) < Double.Epsilon)
+                throw new DivideByZeroException("Cannot divide by zero");
+            return Multiply(a, 1 / b);
+        }
+    }
+}
